38534a2fd22d49eb8fd119f7d3d33bee3afee661
Ling <loveas6632@gmail.com>
2025-02-20 10:30:27 +0800

    composer require laravel/socialite

---
+++ text-20250211/app/Http/Controllers/MerchandiseController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Hash;
+use App\Shop\Entity\User;
+use Illuminate\Support\Facades\Mail;
+use App\Shop\Entity\Merchandise;
+
+class MerchandiseController extends Controller
+{
+
+    public function MerchandiseCreateProcess()
+    {
+        $merchandise_data = [
+            'status' => 'C',
+            'name' => '',
+            'name_en' => '',
+            'introduction' => '',
+            'introduction_en' => '',
+            'photo' => '',
+            'price' => 0,
+            'remain_count' => 0,
+        ];
+
+        $merchandise_sql_data = Merchandise::create($merchandise_data);
+
+        return redirect('/merchandise/' . $merchandise_sql_data['id'] . '/edit');
+    }
+
+    public function MerchandiseEditPage($merchandise_id)
+    {
+        $merchandises = Merchandise::where('id', $merchandise_id);
+        if ($merchandises->count() === 0) {
+            return redirect('/');
+        } else {
+            $merchandise = $merchandises->first();
+
+            $binding = [
+                'title' => '編輯商品',
+                'merchandise' => $merchandise
+            ];
+            return view('merchandise.edit', $binding);
+        }
+    }
+
+    public function MerchandiseEditProcess($merchandise_id)
+    {
+        $input = request()->all();
+
+        unset($input['_token']);
+
+        if (isset($input['photo'])) {
+            // 有上傳圖片
+            $photo = $input['photo'];
+            // 檔案副檔名
+            $file_extension = $photo->getClientOriginalExtension();
+            // 產生自訂隨機檔案名稱
+            $file_name = uniqid() . '.' . $file_extension;
+            // 檔案相對路徑
+            $file_relative_path = 'images/merchandise/';
+            // 檔案存放目錄為對外公開 public 目錄下的相對位置
+            $file_path = public_path($file_relative_path);
+            // 移動上傳檔案
+            $photo->move($file_path, $file_name);
+            // 設定存入資料庫的檔案路徑 是相對路徑
+            $input['photo'] = $file_relative_path . $file_name;
+        }
+
+        Merchandise::where('id', $merchandise_id)
+            ->update($input);
+
+        return redirect('/merchandise/' . $merchandise_id . '/edit');
+    }
+public function MerchandiseManagePage()
+{
+    $merchandises = Merchandise::get();
+    $binding = [
+        'title' => '管理商品',
+        'merchandises' => $merchandises
+    ];
+    return view('merchandise.manage', $binding);
+}
+
+
+
+}
+
+   
+
+
+
+
+
--- text-20250211/resources/views/auth/signin.blade.php
+++ text-20250211/resources/views/auth/signin.blade.php
@@ -11,23 +11,13 @@
 <!-- @include('component.social') -->
 @include('component.errors')
 
-<form action="/user/auth/signup" method="post">
+<form action="/user/auth/signin" method="post">
     {{ csrf_field() }}
-    暱稱： <input type="text" name="nickname"
-        placeholder="暱稱" value="{{old('nickname')}}"><br>
+
     Email： <input type="text" name="email"
         placeholder="Email" value="{{old('email')}}"><br>
     密碼： <input type="password" name="password"
         placeholder="密碼" value="{{old('password')}}"><br>
-    使用者類型：
-    <input type="radio" name="type" value="G"
-        @if (old("type")=="G" )
-        checked
-        @endif> 一般使用者
-    <input type="radio" name="type" value='A'
-        @if (old("type")=="A" )
-        checked
-        @endif> 管理員<br>
-    <input type="submit" value="註冊">
+    <input type="submit" value="登入">
 </form>
 @endsection
